e0c44b5ee2f4c2e6a6b6dd65a25adbe5473dd59e
AdrasteaCityGit <156355608+AdrasteaCityGit@users.noreply.github.com>
2024-02-21 20:48:44 +0000

    up on the script to a simple version

--- projects/acg_eugenia/acg_eugenia.py
+++ projects/acg_eugenia/acg_eugenia.py
@@ -6,7 +6,7 @@ def extract_metadata(image_path):
     with Image.open(image_path) as img:
         exif_data = img._getexif()
 
-        if exif_data in not None:
+        if exif_data is not None:
             mapped_exif_data = {ExifTags.TAGS[key]: exif_data[key] for key in exif_data.keys() if key in ExifTags.TAGS}
             return mapped_exif_data
         else: 
@@ -16,6 +16,8 @@ def main():
     parser = argparse.ArgumentParser(description="Extract EXIF metadata from an image.")
     parser.add_argument("image", type=str, help="Path to the input image.")
     args = parser.parse_args()
+
+    image_path = args.image
 
     if not os.path.exists(image_path):
         print(f"Error: Image file not found: {image_path}")
@@ -29,5 +31,5 @@ def main():
     else:
         print(f"No metadata found for {image_path}")
 
-if __name__ = "main":
+if __name__ == "__main__":
     main()
