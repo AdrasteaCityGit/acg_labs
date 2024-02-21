2a9e66fd07cd91987c802377f860d9e19f8c4934
AdrasteaCityGit <156355608+AdrasteaCityGit@users.noreply.github.com>
2024-02-21 04:46:10 +0000

    up

--- projects/acg_eugenia/acg_eugenia.py
+++ projects/acg_eugenia/acg_eugenia.py
@@ -102,19 +102,30 @@ class ArchiveMetadataExtractor:
         with rarfile.RarFile(self.file_path, 'r') as rar_file:
             self._extract_metadata_from_archive(rar_file, selected_files, filter_types, extract_content, advanced_analysis)
 
-    def extract_pdf_metadata(self):
-    with open(self.file_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        info = pdf_reader.getDocumentInfo()
-        self.metadata['PDF_Metadata'] = {
-            'title': info.title,
-            'author': info.author,
-            'subject': info.subject,
-            'producer': info.producer,
-            'created_date': info.created.strftime('%Y-%m-%d %H:%M:%S UTC'),
-            'modified_date': info.modified.strftime('%Y-%m-%d %H:%M:%S UTC'),
-            'number_of_pages': len(pdf_reader.pages)
-        }
+        def _extract_metadata(self, info, selected_files=None, filter_types=None, extract_content=False, advanced_analysis=False):
+        if not selected_files or info.filename in selected_files:
+            file_type = self.identify_file_type(info.filename)
+            if filter_types and file_type not in filter_types:
+                return
+
+            if file_type not in self.metadata:
+                self.metadata[file_type] = {}
+
+            self.metadata[file_type][info.filename] = self.get_common_metadata(info)
+            if hasattr(info, 'compress_type'):
+                self.metadata[file_type][info.filename]['compression_type'] = info.compress_type
+
+            # Call specific extraction method based on file type
+            extraction_method = getattr(self, f'extract_{file_type.lower()}_metadata', None)
+            if extraction_method:
+                extraction_method(info.filename)
+
+            if extract_content:
+                self.extract_content(info.filename, file_type)
+
+                if advanced_analysis:
+                    self.perform_advanced_analysis(info.filename, file_type)
+
 
 
     def extract_image_metadata(self, file_name):
